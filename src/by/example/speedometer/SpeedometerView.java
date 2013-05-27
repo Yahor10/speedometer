1edeea7a2443f9bc53da6fbad09cc9dbe35637a6
Egor <sekt88@gmail.com>
2013-05-27 10:32:50 +0300

    implement speed hand

--- src/by/example/speedometer/SpeedometerView.java
+++ src/by/example/speedometer/SpeedometerView.java
@@ -1,18 +1,18 @@
 package by.example.speedometer;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Path;
-import android.graphics.Path.Direction;
-import android.graphics.PathDashPathEffect;
-import android.graphics.PathEffect;
-import android.graphics.PathMeasure;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 
@@ -27,6 +27,14 @@ public class SpeedometerView extends android.view.View {
 	private int speedSteps;
 	private static String TAG = "SIMPLEDROID";
 
+	Path path;
+	Path speedSmallPath;
+	Path speedLargePath;
+	private Bitmap bitmap;
+	private Drawable speedPointer;
+
+	private static RectF ovalRectOUT = new RectF(0, 0, 50, 50);
+	private static RectF speedRect = new RectF(0, 0, 50, 50);
 	public SpeedometerView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
@@ -49,63 +57,96 @@ public class SpeedometerView extends android.view.View {
 
 		paintRect = new Paint();
 		paintRect.setStyle(Style.FILL);
-		paintRect.setColor(Color.RED);
+		paintRect.setColor(Color.GREEN);
+		
+		path = new Path();
+		speedSmallPath = new Path();
+		speedLargePath = new Path();
+
+		Resources res = getResources();
+		speedPointer = res.getDrawable(R.drawable.clockgoog_minute);
+		bitmap = BitmapFactory.decodeResource(res, R.drawable.clockgoog_minute);
+	}
+	
+	@Override
+	protected void onAttachedToWindow() {
+		Log.i(TAG, "onAttachedToWindow");
+		super.onAttachedToWindow();
+	}
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		Log.i(TAG, "onMeasure");
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+	
+	@Override
+	protected void onLayout(boolean changed, int left, int top, int right,
+			int bottom) {
+		Log.i(TAG, "onLayout");
+		super.onLayout(changed, left, top, right, bottom);
 	}
 
 	@Override
+	public void requestLayout() {
+		Log.i(TAG, "requestLayout");
+		super.requestLayout();
+	}
+	
+
+	@Override
 	protected void onDraw(Canvas canvas) {
-		Log.v(TAG, "ON DRAW");
+		Log.i(TAG, "ON DRAW");
 		super.onDraw(canvas);
-		Path path = new Path();
-		Path speedSmallPath = new Path();
-		Path speedLargePath = new Path();
 
-		RectF ovalRectOUT = new RectF(0, 0, 50, 50);
-		RectF speedRect = new RectF(0, 0, 50, 50);
-
-		float semicircle = getSemicircle(10, 100, 200, 100, ovalRectOUT,
+		getSemicircle(10, 100, 200, 100, ovalRectOUT,
 				Side.LEFT);
-		float semicircleSpeed = getSemicircle(15, 100, 195, 100, speedRect,
+		getSemicircle(15, 100, 195, 100, speedRect,
 				Side.LEFT);
 
-		path.addArc(ovalRectOUT, semicircle, 180);
+		int sweepAngle = 220;
+		int startAngle = 160;
+		path.addArc(ovalRectOUT, startAngle, sweepAngle);
 
-		speedSmallPath.addArc(speedRect, semicircleSpeed, 180);
-		speedLargePath.addArc(speedRect, semicircleSpeed, 180);
+		// TODO investigate correct sweep
+		speedSmallPath.addArc(speedRect, startAngle, sweepAngle);
+		speedLargePath.addArc(speedRect, startAngle, sweepAngle);
 
-		PathMeasure pm = new PathMeasure(path, false);
-		float aCoordinates[] = { 0f, 0f };
 		// get point from the middle
-		boolean posTan = pm
-				.getPosTan(pm.getLength() * 0.5f, aCoordinates, null);
-		Log.v(TAG, "COORDS" + aCoordinates[0] + " " + aCoordinates[1]);
-		Log.v(TAG, "RECT " + ovalRectOUT);
 
 		canvas.drawRect(ovalRectOUT, paintRect);
 		canvas.drawPath(path, paintFill);
 
 		int speedSpot = 0;
 		int vOffsetStep = 0;
-		for (int i = 0; i < 13; i++) {
+		for (int i = 0; i < 18; i++) {
 			canvas.drawTextOnPath(Integer.toString(speedSpot), path,
 					vOffsetStep, 20, paintText);
 			speedSpot += 10;
 			vOffsetStep += 20;
 		}
 
-		float[] pathEffSmall = {5,5}; // 5 - width ,30 - padding
-		float[] pathEffLarge = {5,15}; // 5 - width ,30 - padding
-		paintLargeSpeed.setPathEffect(new DashPathEffect(pathEffLarge,0));
-		paintSmallSpeed.setPathEffect(new DashPathEffect(pathEffSmall,0));
+		float[] pathEffSmall = { 5, 5 }; // 5 - width ,5 - padding
+		float[] pathEffLarge = { 5, 15 }; // 5 - width ,15 - padding
+		
+		paintLargeSpeed.setPathEffect(new DashPathEffect(pathEffLarge, 0));
+		paintSmallSpeed.setPathEffect(new DashPathEffect(pathEffSmall, 0));
+		
 		canvas.drawPath(speedSmallPath, paintSmallSpeed);
-		canvas.drawPath(speedLargePath, paintLargeSpeed);		
-
-	}
-
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		Log.v(TAG, "onMeasure");
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		canvas.drawPath(speedLargePath, paintLargeSpeed);
+		
+		int intrinsicWidth = speedPointer.getIntrinsicWidth();
+		int intrinsicHeight = speedPointer.getIntrinsicHeight();
+		
+		Log.v(TAG,"width" + intrinsicWidth);
+		Log.v(TAG,"height" + intrinsicHeight);
+		
+		canvas.save();
+		canvas.rotate(360,100,100);// TODO calculate rotate
+		speedPointer.setBounds(100 - (intrinsicWidth / 2), 110 - (intrinsicHeight / 2), 100 + (intrinsicWidth / 2), 60 + (intrinsicHeight / 2));
+		speedPointer.draw(canvas);
+		
+		
+		canvas.restore();
 	}
 
 	public static Path getEquilateralTriangle(Point p1, int width,
@@ -156,8 +197,9 @@ public class SpeedometerView extends android.view.View {
 		} else {
 			radStartAngle = Math.atan2(yEnd - centerY, xEnd - centerX);
 		}
+		Log.v(TAG, "RAD" + radStartAngle);
 		float startAngle = (float) Math.toDegrees(radStartAngle);
-
+		Log.v(TAG, "Degrees" + startAngle);
 		return startAngle;
 
 	}
